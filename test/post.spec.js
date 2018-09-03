9fdae08a088a8e850a198936ccb1f81b2fae22aa
LucHighwalker <luc.highwalker@gmail.com>
2018-09-02 23:13:03 -0700

    - Added post test.

---
+++ test/post.spec.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const database = require('../database');
+
+const Post = require('../models/post');
+
+describe('Posts', () => {
+    it('should create with valid attributes at POST /post', (done) => {
+        database.getAll(Post).then((posts) => {
+            var postCount = posts.count;
+          
+            var post = { title: "post title", url: "https://www.google.com", summary: "post summary" }
+            chai.request('localhost:4200')
+              .post('/test', post)
+              .end(function (err, res){
+
+                if (err) {
+                    console.error(err);
+                    done();
+                }
+
+                database.getAll(Post).then((posts) => {
+                    var newCount = posts.count;
+                    newCount.should.be.equal(postCount + 1);
+
+                    res.should.have.status(200);
+                    done();
+                }).catch((error) => {
+                    console.error(error);
+                    done();
+                });
+            });
+          }).catch((error) => {
+              console.error(error);
+              done();
+          });
+    });
+});
