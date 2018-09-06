e38b53055184cc2c33c6466c0e09e47267ec8ed2
LucHighwalker <luc.highwalker@gmail.com>
2018-09-06 09:30:18 -0700

    - Proper route nesting.

---
+++ routers/posts.js
@@ -0,0 +1,59 @@
+const posts = require('express').Router();
+
+const bodyParser = require('body-parser');
+const database = require('../controllers/database');
+
+const PostModel = require('../models/post');
+
+urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+posts.get('/', (req, res) => {
+    var search = req.search;
+
+    database.getAll(PostModel, search).then((response) => {
+        res.render('posts', {
+            posts: response
+        });
+    }).catch((error) => {
+        console.error(error);
+        res.render('error');
+    });
+});
+
+posts.get('/create', (req, res) => {
+    res.render('posts-new');
+});
+
+posts.post('/new', urlEncodedParser, (req, res) => {
+    var post = new PostModel(req.body);
+
+    database.save(post).then(() => {
+        res.render('posts-success');
+    }).catch((error) => {
+        console.error(error);
+    });
+});
+
+posts.post('/del', urlEncodedParser, (req, res) => {
+    var post = new PostModel(req.body);
+    var id = post._id;
+
+    console.log(post);
+
+    database.del(PostModel, id).then(() => {
+        res.render('posts-success');
+    }).catch((error) => {
+        console.error(error);
+    });
+});
+
+posts.get('/:id', function (req, res) {
+    var id = req.params.id;
+    database.getOne(PostModel, id).then((post) => {
+        res.render('post-show.hbs', { post });
+    }).catch((error) => {
+        console.error(error);
+    });
+});
+
+module.exports = posts;
--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const database = require('./database');
-const PostModel = require('./models/post');
 
-urlEncodedParser = bodyParser.urlencoded({extended: false});
+const postRouter = require('./routers/posts');
 
 // App initialization
 app.engine('hbs', exphbs({
@@ -29,51 +26,7 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/posts', (req, res) => {
-    var search = req.query.search;
-
-    database.getAll(PostModel, search).then((response) => {
-        res.render('posts', {
-            posts: response
-        });
-    }).catch((error) => {
-        console.error(error);
-        res.render('error');
-    });
-});
-
-app.get('/posts/create', (req, res) => {
-    res.render('posts-new');
-});
-
-app.post('/posts/new', urlEncodedParser, (req, res) => {
-    var post = new PostModel(req.body);
-
-    database.save(post).then(() => {
-        res.render('posts-success');
-    }).catch((error) => {
-        console.error(error);
-    });
-});
-
-app.post('/posts/del', urlEncodedParser, (req, res) => {
-    var post = new PostModel(req.body);
-    var id = post._id;
-
-    console.log(post);
-
-    database.del(PostModel, id).then(() => {
-        res.render('posts-success');
-    }).catch((error) => {
-        console.error(error);
-    });
-});
-
-app.get('/posts/:id', function (req, res) {
-    var id = req.params.id;
-    database.getOne(PostModel, id).then((post) => {
-        res.render('post-show.hbs', { post });
-    }).catch((error) => {
-        console.error(error);
-    });
-});
+app.use('/posts', (req, res, next) => {
+    req.search = req.query.search;
+    next();
+}, postRouter);
